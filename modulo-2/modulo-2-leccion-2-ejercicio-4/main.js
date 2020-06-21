2d73be08292cdc1effeb6509274150272d2e9ef6
Christina <mchristinalorenzomontes>
2020-06-21 12:52:36 +0200

    Add module-2-leccion-2-ejercicio-4

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-4/main.js
@@ -0,0 +1,7 @@
+const billforall = (128 - 2) / 9;
+const Ana = billforall + 2;
+console.log(billforall);
+console.log(Ana);
+
+//Nos vamos de cena de Navidad, ¡qué alegría! Somos en total 9 personas y la cuenta del restaurante japonés es de 128€. Ana tiene que pagar 2€ más que los demás porque ha pedido un chupito de sake. ¿Cuánto tenemos que pagar cada una? ¿Y Ana? Hagamos un pequeño programa en JavaScript para calcularlo. El resultado debe mostrarse en la consola del navegador.//
+//Ana tiene que pagar 1y y el resto 14//
