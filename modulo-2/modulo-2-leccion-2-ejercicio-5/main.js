9da547fa1860a0ba8d248c2412d5ba31349f3a7c
Christina <mchristinalorenzomontes>
2020-06-21 13:00:36 +0200

    Add modulo-2-leccion-2-exercise-5

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-5/main.js
@@ -0,0 +1,7 @@
+const years = 29;
+const hoursperday = 24;
+const daysperyear = 365;
+console.log(years * hoursperday * daysperyear);
+
+//Calcuala el número total de horas vividas//
+//Tengo 29 años por lo que aproximadamente he vivido 254 040‬ horas//
