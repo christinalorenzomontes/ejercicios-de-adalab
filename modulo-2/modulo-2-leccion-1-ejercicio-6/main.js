26e9e7ea9d9fb784e5fe7fc0aac2c85b2060fa2d
Christina <mchristinalorenzomontes>
2020-06-21 12:26:51 +0200

    Add modulo-leccion-2-exercise-3

---
+++ modulo-2/modulo-2-leccion-1-ejercicio-6/main.js
@@ -0,0 +1,4 @@
+const titleElement = document.querySelector(".selection");
+const passwordElement = document.querySelector(".asterik2");
+titleElement.innerHTML =
+  titleElement.innerHTML + " " + passwordElement.innerHTML;
---
+++ modulo-2/modulo-2-leccion-3-ejercicio-3/main.js
@@ -0,0 +1,4 @@
+const pricekiwis = 5 * 2;
+const pricepears = 2 * 3;
+const pricegrapes = 4 * 0.5;
+console.log(pricekiwis + pricepears + pricegrapes);
