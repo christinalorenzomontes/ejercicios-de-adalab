4a740b24e828615cf6152c9be93cb5fba73e0d08
Christina <christinalorenzomontes@gmail.com>
2020-06-25 16:20:30 +0200

    Add 2.5.5

--- modulo-2/modulo-2-leccion-5-ejercicio-3/main.js
+++ modulo-2/modulo-2-leccion-5-ejercicio-3/main.js
@@ -3,10 +3,7 @@
 const loremIpsum = document.querySelector(".ipsum");
 
 function duplicateIpsum() {
-  let loremIpsum2 = document.querySelector(".ipsum");
-
-  loremIpsum2.innerHTML =
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantimum ullam unde expedita sapiente dolore facere. Voluptate esse quidem dolore quo quis minus sit, dolor porro tempore, dicta, vel cupiditate architectoLorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantimum ullam unde expedita sapiente dolore facere. Voluptate esse quidem dolore quo quis minus sit, dolor porro tempore, dicta, vel cupiditate architecto.";
+  loremIpsum.innerHTML = loremIpsum.innerHTML + loremIpsum.innerHTML;
 }
 
 loremIpsum.addEventListener("mouseover", duplicateIpsum);
