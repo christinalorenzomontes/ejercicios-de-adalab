d9cdd804da6bca6962248784e3ef55b9a5381818
Christina <christinalorenzomontes@gmail.com>
2020-06-28 16:32:54 +0200

    Add 2.7.7

--- modulo-2/modulo-2-leccion-7-ejercicio-6/main.js
+++ modulo-2/modulo-2-leccion-7-ejercicio-6/main.js
@@ -1,12 +1,25 @@
 "use strict";
-// SIN COMPLETAR
-const button = document.querySelector(".js-button");
-const film = document.querySelector(".movie");
 
-// handler
-function showMovie() {
-  alert(`¡A mí también me encantaron ${film}.value!`);
+// Constante que hace referencia a botón
+const button = document.querySelector(".js-button");
+
+// Función handler de qué pasa cuando clickeamos el botón
+function showMessage() {
+  // La constante que recoge el valor que hemos introducido en el input la declaramos una vez hemos pulsado el botón. Esto es importante, si la declaramos arriba debajo de la de button, su valor será el que por defecto tiene el value de un input vacío. --> queremos que el valor se le asigne una vez hemos escrito los libros y pulsado el botón
+
+  // Creamos dos constantes que recogen las dos pelis introducidas
+  const movieInput1 = document.querySelector("#movie1").value;
+  const movieInput2 = document.querySelector("#movie2").value;
+
+  // Array que tendrá como elementos las dos pelis
+  const movieList = [movieInput1, movieInput2];
+
+  // Crear for...of loop que va a mostrar un mensaje por cada libro:
+  for (const movies of movieList) {
+    console.log(
+      `¡A mí también me encantó "${movies}"! Tenemos mucho en común, humana.`
+    );
+  }
 }
 
-// listener sobre el elemento, con tipo de evento y handler
-button.addEventListener("click", showMovie);
+button.addEventListener("click", showMessage);
