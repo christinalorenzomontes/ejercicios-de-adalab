1db8b7d61fa46252c1d0bee14272be91ef291469
Christina <christinalorenzomontes@gmail.com>
2020-07-04 12:34:03 +0200

    Add 2.10.1

---
+++ modulo-2/modulo-2-leccion-10-ejercicio-1/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function getRandomNumber() {
+  fetch("https://api.rand.fun/number/integer?length=20")
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".js-result").innerHTML = data.result;
+    });
+}
+document.querySelector(".js-number").addEventListener("click", getRandomNumber);
