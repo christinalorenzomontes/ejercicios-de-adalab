b3d27f184550d28403a092ee7b2fbf304c94051a
Christina <christinalorenzomontes@gmail.com>
2020-07-04 13:19:26 +0200

    Add 2.10.2

--- modulo-2/modulo-2-leccion-10-ejercicio-1/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-1/main.js
@@ -1,6 +1,6 @@
 "use strict";
 function getRandomNumber() {
-  fetch("https://api.rand.fun/number/integer?length=20")
+  fetch("https://api.rand.fun/number/integer")
     .then((response) => response.json())
     .then((data) => {
       document.querySelector(".js-result").innerHTML = data.result;
