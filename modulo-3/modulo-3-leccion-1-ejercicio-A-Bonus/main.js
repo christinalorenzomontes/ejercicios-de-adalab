a4ab318c0674a48cfd5c8026f0040c42d0723864
Christina <christinalorenzomontes@gmail.com>
2020-07-19 14:30:24 +0200

    Add 3.1.Bonus

--- modulo-3/modulo-3-leccion-1-ejercicio-A-Bonus/main.js
+++ modulo-3/modulo-3-leccion-1-ejercicio-A-Bonus/main.js
@@ -1,30 +1,59 @@
 "use strict";
-//Vamos a crear un programita que haga cálculos geométricos sencillos. En el programa definiremos la clase Square,
-//que tendrá al menos:
-//Un método para calcular el perímetro (perimeter()) del cuadrado
-//(multiplica la longitud del lado por el número de lados)
-//Un método para calcular el área (area()) del cuadrado (elevar al cuadrado el lado)
-//Los métodos recibirán la longitud del lado (side) como parámetro.
-//Calcularemos y mostraremos el perímetro y el área de un cuadrado de 9 de lado.
-class Dog {
-  constructor(name) {
-    this.name = name;
+//Vamos a mejorar ¡aún más! nuestra calculadora geométrica sencilla. En el programa definiremos la clase Polygon,
+//Square y Triangle para polígonos regulares(todos sus lados miden lo mismo).
+//Ya os imagináis por dónde van los tiros: las clases Square y Triangle serán subclases de Polygon.
+//La clase Polygon tendrá, al menos:
+//Un atributo para el número de lados (numberOfSides)
+//Dos atributos más: para la longitud de la base (base) y de la altura (height)
+//Un método para calcular el perímetro (perimeter) (multiplicar la longitud del lado base por el número de lados)
+//Un método para calcular el área (area) (multiplicar base por altura)
+//La subclase Square tendrá:
+//Un atributo lado (side) igual a la base
+//Un constructor que recibirá exclusivamente la longitud del lado
+//La subclase Triangle tendrá:
+//Un constructor que recibirá base y altura
+//Un método area() que sobrescibirá al de la clase base.
+//Devolverá la mitad de lo que devuelva llamar al área de la clase base(super.area() / 2)
+//Crearemos dos instancias: un cuadrado de 4 de lado y un triángulo de 4 de base y 3 de altura.
+//Llamaremos a los dos métodos en todas las instancias.
+
+class Polygon {
+  constructor(numberofSides, base, height) {
+    this.numberofSides = numberofSides;
+    this.base = base;
+    this.height = height;
   }
 
-  bark() {
-    console.log("Woof, woof!");
+  perimeter() {
+    return this.numberofSides * this.base;
   }
 
-  reactToCall(shout) {
-    if (shout.includes(this.name)) {
-      console.log(`${this.name} wags its tail, happily.`);
-    } else {
-      this.bark();
-    }
+  area() {
+    return this.base * this.height;
   }
 }
 
-const laika = new Dog("Laika");
+class Square extends Polygon {
+  constructor(side) {
+    super(4, side, side);
+    this.side = side;
+  }
+}
 
-laika.reactToCall("Hey, Laika!"); // 'Laika wags its tail, happily.'
-laika.reactToCall("Hey, Hachiko!"); // 'Woof, woof!'
+class Triangle extends Polygon {
+  constructor(base, height) {
+    super(3, base, height);
+  }
+  area() {
+    return super.area() / 2;
+  }
+}
+
+const square = new Square(4);
+const triangle = new Triangle(4, 3);
+
+console.log(`El cuadrado tiene un perimetro de ${square.perimeter()}cm`);
+console.log(`El cuadrado tiene un área de ${square.area()}cm`);
+
+console.log(`El triángulo tiene un perimetro de ${triangle.perimeter()}cm`);
+console.log(`El triángulo tiene un área de ${triangle.area()}cm`);
